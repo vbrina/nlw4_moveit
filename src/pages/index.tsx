d875a662a033870ecfca16dff1d7fc63bca5fe58
Vitor Petruz Brina <vitor.brina@venturus.org.br>
2021-02-23 14:38:47 -0300

    Second class done.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { CompletedChallenges } from "../components/CompletedChallenges/CompletedChallenges";
+import { Countdown } from "../components/Countdown/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar/ExperienceBar";
 import { Profile } from "../components/Profile/Profile";
+import Head from "next/head";
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
